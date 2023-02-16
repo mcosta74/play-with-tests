d1ebc726e219c495a68551d22a9844181ca8e1a9
Massimo Costa <massimoc@filewave.com>
2023-02-16 09:28:43 +0100

    add package

---
+++ utils/foo.go
@@ -0,0 +1,5 @@
+package utils
+
+func Hello(name string) string {
+	return "Hello " + name
+}
---
+++ utils/foo_test.go
@@ -0,0 +1,23 @@
+package utils
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Massimo", args{"Massimo"}, "Hello Massimo"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Hello(tt.args.name); got != tt.want {
+				t.Errorf("Hello() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
